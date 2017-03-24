fe592218d0174cb20be4a9edbd6bebe19d636886
lmb543 <lmb543@naver.com>
2017-03-24 19:55:08 +0900

    코드5

---
+++ 키보드조작/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 키보드조작
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_KeyDown(object sender, KeyEventArgs e)
+		{
+			MessageBox.Show("키보드가 눌러졌습니다.");
+		}
+	}
+}
